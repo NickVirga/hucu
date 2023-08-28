e0f3cf164753d9d71da4d9ce044b1f788fd56d01
Nick Virga <nsvirga@gmail.com>
2023-08-28 14:46:51 -0400

    rc-tree library installed, option tree adde with placeholder added

--- src/pages/InquiryPage/InquiryPage.js
+++ src/pages/InquiryPage/InquiryPage.js
@@ -1,7 +1,65 @@
-import './InquiryPage.scss';
+import "./InquiryPage.scss";
+import Tree from "rc-tree";
+import "rc-tree/assets/index.css"
 
 function InquiryPage() {
-  return
+  const treeData = [
+    {
+      key: "1",
+      title: "Food",
+      children: [
+        {
+          key: "2",
+          title: "Burger",
+        },
+        {
+          key: "3",
+          title: "Salad",
+          children: [
+            {
+              key: "4",
+              title: "Tomatoes",
+            },
+            {
+              key: "5",
+              title: "Cabbage",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      key: "6",
+      title: "Drinks",
+      children: [
+        {
+          key: "7",
+          title: "Beer",
+        },
+        {
+          key: "8",
+          title: "Soft drink",
+        },
+      ],
+    },
+  ];
+
+  const selectHandler= (selectedKeys, { node }) => {
+    console.log(node)
+  }
+
+
+  return (
+    <div className="inquiries__container">
+      <Tree
+        showLine
+        selectable
+        treeData={treeData}
+        onSelect={selectHandler}
+        showIcon={false}
+      />
+    </div>
+  );
 }
 
 export default InquiryPage;
