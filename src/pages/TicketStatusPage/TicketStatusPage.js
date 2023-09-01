7fe1e2114c1f080e84346bd5ec7cbaa6fcd3d5ef
Nick Virga <nsvirga@gmail.com>
2023-09-01 13:45:29 -0400

    basic content of ticket status page added

--- src/pages/TicketStatusPage/TicketStatusPage.js
+++ src/pages/TicketStatusPage/TicketStatusPage.js
@@ -1,7 +1,33 @@
-import './TicketStatusPage.scss';
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { urlTicketById } from "../../utils/api-utils";
+
+import "./TicketStatusPage.scss";
 
 function TicketStatusPage() {
-  return
+  const { ticketId } = useParams();
+  const [ticket, setTicket] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(urlTicketById(ticketId))
+      .then((response) => {
+        setTicket(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Ticket #{ticket.id}</h1>
+      <h2>Your ticket is {ticket.status}.</h2>
+      <p>You are in queue</p>
+      <p>{ticket.queue_number} people ahead of you</p>
+    </div>
+  );
 }
 
 export default TicketStatusPage;
